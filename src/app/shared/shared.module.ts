44d791bbeea6cbf5fcb60e413a48b3a7e98a8f1b
Guv <Guv@thehouseshop.com>
2020-10-09 13:31:07 +0100

    mat-button-raised was not being applied to other modules - fixed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SectionContentComponent } from '../_elements/section-content/section-content.component';
 import { RouterModule } from '@angular/router';
 import { BootstrapCarouselComponent } from '../_elements/bootstrap-carousel/bootstrap-carousel.component';
-
-
+import { MatButtonModule, MatIconModule, MatTooltipModule, MatToolbarModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [SectionContentComponent, BootstrapCarouselComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   exports: [SectionContentComponent, BootstrapCarouselComponent]
 })
